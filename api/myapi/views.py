f6ecbfac36d8992c372addeca6b0f1253ca04630
yeemaae <noobproger@gmail.com>
2024-04-01 14:33:18 +0600

    test Commit

--- api/myapi/views.py
+++ api/myapi/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
 def index(reqirements):
-    return HttpResponse("Hello world")
+    return HttpResponse("Hello!!!!")
