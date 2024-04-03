f76df3457c4b9a0e172747b1b383ecbdc7a25e4d
yeemaae <noobproger@gmail.com>
2024-04-03 12:09:38 +0600

    added student&teacher

--- api/myapi/urls.py
+++ api/myapi/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.index),
-    path('about/', views.about),
+    path('', views.index),
+    path('teachers/', views.teacher),
+    path('students/', views.student),
 ]
--- api/myapi/views.py
+++ api/myapi/views.py
@@ -3,14 +3,51 @@ from django.http import HttpResponse, JsonResponse
 from .models import DataBase
 from django.core.serializers import serialize
 
+data_base = DataBase.objects.all()
+
 
 # Create your views here.
 def index(request):
-    data_base = DataBase.objects.all()
     field_names = [field.name for field in DataBase._meta.fields]
     data = [{field_name: getattr(obj, field_name) for field_name in field_names} for obj in data_base]
     return JsonResponse(data, safe=False)
 
 
-def about(request):
-    return HttpResponse("Page About Us!!!!")
+def teacher(request):
+    try:
+        # Fetch all field names for the model
+        field_names = [field.name for field in DataBase._meta.fields]
+
+        # Fetch data from the database and construct dictionaries for each row
+        data = [{field_name: getattr(obj, field_name) for field_name in field_names} for obj in DataBase.objects.all()]
+
+        # Filter data based on the value of 'send_to_nobd' field
+        filtered_data = [row for row in data if row.get('send_to_nobd') == 2]
+
+        # Return the filtered data as JSON response
+        return JsonResponse(filtered_data, safe=False)
+    except Exception as e:
+        # Print or log the exception for debugging
+        print("An error occurred:", str(e))
+        # Return an error response
+        return JsonResponse({'error': 'An error occurred'}, status=500)
+
+
+def student(request):
+    try:
+        # Fetch all field names for the model
+        field_names = [field.name for field in DataBase._meta.fields]
+
+        # Fetch data from the database and construct dictionaries for each row
+        data = [{field_name: getattr(obj, field_name) for field_name in field_names} for obj in DataBase.objects.all()]
+
+        # Filter data based on the value of 'send_to_nobd' field
+        filtered_data = [row for row in data if row.get('send_to_nobd') == 1]
+
+        # Return the filtered data as JSON response
+        return JsonResponse(filtered_data, safe=False)
+    except Exception as e:
+        # Print or log the exception for debugging
+        print("An error occurred:", str(e))
+        # Return an error response
+        return JsonResponse({'error': 'An error occurred'}, status=500)
